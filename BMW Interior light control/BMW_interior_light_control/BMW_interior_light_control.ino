3eb5383d77f919eceb7663a284c7794e175b1efc
Raku5200 <31960330+Raku5200@users.noreply.github.com>
2017-09-14 18:53:08 +0530

    Add files via upload

---
+++ BMW Interior light control/BMW_interior_light_control/BMW_interior_light_control.ino
@@ -0,0 +1,38 @@
+// pin 1 input from door (open-) 1k pull up
+// pin 0 out interior lights-
+// pin 2 (A1) Input From LDR
+
+int HYS = 10;
+int ledPin1 = 0;
+int LDR1 = A1;
+int LDRValue1 = 0;
+int light_sensitivity1 = 25;
+int light1 = 0;
+void setup()
+ {
+  pinMode( 1, INPUT);
+ }
+void loop()
+{
+LDRValue1 = analogRead(LDR1);
+int light1 = map(LDRValue1, 0, 1012, 0, 100);
+static int fadeValue1 = 0;
+ if ((light1 < light_sensitivity1 + HYS) && (digitalRead(1) == LOW))
+  {
+   if (fadeValue1 < 255)
+   {
+    fadeValue1++;
+     analogWrite(ledPin1,fadeValue1);
+     delay(15);
+   }
+  }
+if ((light1 > light_sensitivity1 - HYS) || (digitalRead(1) == HIGH))
+ {
+  if (fadeValue1 > 0)
+   { fadeValue1--;
+    analogWrite(ledPin1,fadeValue1);
+    delay(20);
+   }
+  }
+}
+
